a2b54a6fe8e99a15a3bfef527898cd53fe57daae
tainguyen <tainguyendhhh@gmail.com>
2025-11-15 23:17:28 +0900

    sửa bài điểm dinh viên

--- ss7_bien_kieu_du_lieu/bai_tap/diem_sinh_vien.js
+++ ss7_bien_kieu_du_lieu/bai_tap/diem_sinh_vien.js
@@ -1,11 +1,11 @@
 
 
 function tong() {
-    let diemtoan= +document.getElementById("diemtoan").value;
-    let diemhoa= +document.getElementById("diemhoa").value;
-    let diemsinh= +document.getElementById("diemsinh").value;
-    let tBinh= (diemtoan+ diemsinh+diemhoa)/3;
-    let tong1= (diemtoan+ diemsinh+diemhoa);
+    let diemToan= +document.getElementById("diemtoan").value;
+    let diemHoa= +document.getElementById("diemhoa").value;
+    let diemSinh= +document.getElementById("diemsinh").value;
+    let tBinh= (diemToan+ diemSinh+diemHoa)/3;
+    let tong1= (diemToan+ diemSinh+diemHoa);
     document.getElementById("result").innerHTML=`tổng: `+tong1;
     document.getElementById("result1").innerHTML=`tb: `+tBinh;
 
